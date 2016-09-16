2e11145089ebaa6eb1524c1e74e6fba9befdeaf8
Sudhakar <sudhakar98@gmail.com>
2016-09-16 22:03:31 +0530

    search flight added

--- project/urls.py
+++ project/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
 
     url(r'^$', index),
     url(r'^health$', health),
-    url(r'^searchflight$', searchflight),
+    url(r'^searchflight/', searchflight),
     url(r'^admin/', include(admin.site.urls)),
 ]
