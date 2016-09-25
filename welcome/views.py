bd29d92a090ae854ae39e088e27c100ac0f11158
Sudhakar <sudhakar98@gmail.com>
2016-09-25 23:41:36 +0530

    Sample response

--- welcome/views.py
+++ welcome/views.py
@@ -24,4 +24,6 @@ def health(request):
 
 
 def searchflight(request):
-    return HttpResponse(main.amadeus_flight_search_webhook(request.body))
+    response = "{response=Your query is submitted successfully.}"
+    return HttpResponse(response)
+    #return HttpResponse(main.amadeus_flight_search_webhook(request.body))
