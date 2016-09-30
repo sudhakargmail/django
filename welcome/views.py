cceb239adb62a571f03fdea69531ed0501d3c438
Sudhakar <sudhakar98@gmail.com>
2016-09-30 13:19:58 +0530

    Search flights hardcoded

--- welcome/views.py
+++ welcome/views.py
@@ -24,6 +24,6 @@ def health(request):
 
 
 def searchflight(request):
-    response = """{  "botkitVersion": "0.4.0",  "messages": [ { "_type": "TextMessage", "text": "Hello" } ] }"""
+    response = """{  "botkitVersion": "0.4.0",  "messages": [ { "_type": "TextMessage", "text": "Here are 3 search results" } ] }"""
     return HttpResponse(response)
     #return HttpResponse(main.amadeus_flight_search_webhook(request.body))
